f331b92db2c0b8774681cdf266633f601852d1ae
kiike23 <tupsi.love@gmail.com>
2014-04-23 21:54:18 -0500

    Parte uno de la practica lista, ya se muestra el test de HelloController y el Hello.jsp
    Nota: no modifique el jsp: no quite las directivas
    Nota2: usea otra configuración de las dependencias

--- ejemploapp/java/edu/uv/ejemploapp/web/HelloController.java
+++ ejemploapp/java/edu/uv/ejemploapp/web/HelloController.java
@@ -11,12 +11,8 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 @Controller
-
 public class HelloController {
-
-	
 	protected final Log logger = LogFactory.getLog(getClass());
 
     @RequestMapping(value="/hello.htm")
@@ -26,7 +22,5 @@ public class HelloController {
         logger.info("Returning hello view");
 
         return new ModelAndView("hello.jsp");
-
-
     }
 }
